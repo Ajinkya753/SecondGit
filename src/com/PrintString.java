3bf7e5f195fd7981188cf59246dbe12bf69c19e9
AjinkyaBhosale <ajinkyabhosale009@gmail.com>
2023-01-08 18:28:45 +0530

    First Commit

---
+++ src/com/PrintString.java
@@ -0,0 +1,11 @@
+package com;
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="I can do it!!";
+		System.out.println(str);
+	}
+
+}
